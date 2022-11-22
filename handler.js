4619a0d9eecc7059bc24db22c252870eb8d5b946
Анна Шахбазян <fatamorgana9205@gmail.com>
2022-11-22 19:13:17 +0000

    add rekognition

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@ const AWS = require("aws-sdk");
 const parser = require("lambda-multipart-parser");
 
 const s3 = new AWS.S3();
+const rekognition = new AWS.Rekognition();
 
 async function saveFile(file) {
     console.log({ file });
@@ -14,10 +15,29 @@ async function saveFile(file) {
             Body: file.content,
         })
         .promise();
-    return savedFile;
+    const { Labels } = await rekognition
+        .detectLabels({
+            Image: {
+                Bytes: file.content,
+            },
+        })
+        .promise();
+
+    return {
+        savedFile: `https://${BucketName}.s3.amazonaws.com/${file.filename}`,
+        labels: Labels.map(label => label.Name),
+    };
 }
 
 module.exports.savePhoto = async (event) => {
     const { files } = await parser.parse(event);
-    files.forEach(saveFile);
+    const filesData = files.map(saveFile);
+    const results = await Promise.all(filesData);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            results,
+        }),
+    };
 };

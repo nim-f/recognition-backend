ef30302fe29e005523c552600b4de1b0a9f8da48
Анна Шахбазян <fatamorgana9205@gmail.com>
2023-01-01 20:25:16 +0000

    add pagination

--- functions/getPhotos.js
+++ functions/getPhotos.js
@@ -5,12 +5,21 @@ const { PHOTOS_TABLE } = require("../const/paths");
 const { dynamoDb } = require("../const/providers");
 
 module.exports.getPhotos = async (event) => {
+    const { limit, startKey } = event.queryStringParameters;
+
+    const ExclusiveStartKey = {
+        primary_key: startKey,
+    };
     const results = await dynamoDb
         .scan({
             TableName: PHOTOS_TABLE,
-            Limit: 50,
+            Limit: limit || 10,
+            ...(startKey ? { ExclusiveStartKey } : {}),
         })
         .promise();
-
-    return sendResponse(200, formatPhotoResponse(results.Items));
+        
+    return sendResponse(200, {
+        items: formatPhotoResponse(results.Items),
+        lastKey: results?.LastEvaluatedKey?.primary_key,
+    });
 };
